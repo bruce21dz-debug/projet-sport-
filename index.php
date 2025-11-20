cfc0ba40b59576ee9b92d609c46dce565e0f4a6a
bruce21dz-debug <bruce21dz@gmail.com>
2025-11-20 14:51:13 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,90 +1,33 @@
-
-<?php
-include 'config.php';
-session_start();
-$user_id = $_SESSION['user_id'];
-
-if(!isset($user_id)){
-   header('location:login.php');
-};
-
-if(isset($_GET['logout'])){
-   unset($user_id);
-   session_destroy();
-   header('location:login.php');
-};
-
-
-
-
-?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amiri&family=Cairo:wght@200&family=Poppins:wght@100;200;300&family=Tajawal:wght@300&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site Statique avec Diaporama</title>
-    <link rel="stylesheet" href="css/styleindex.css">
+    <title>Shope online | Ajouter les Produits</title>
+    <link rel="stylesheet" href="CSS/index.css">
 </head>
 <body>
-
-    <header class="header">
-        <div class="logo">Mon Site</div>
-        <nav class="nav">
-            <a href="#accueil" class="nav-btn">Accueil</a>
-            <a href="#services" class="nav-btn">Sports</a>
-            <a href="products.php" class="nav-btn">Billetteris</a>
-
-            <a href="#contact" class="nav-btn primary-btn">Contact</a>
-
-
-
-        </nav>
-    </header>
-
-    <section class="diaporama-container">
-        <h2>Articles à la Une</h2>
-
-        <div class="carousel-wrapper">
-
-            <article class="diaporama-item item-1">
-                <img src="images/cycliste.jpg" alt="Photo de l'article 1" width="100%">
-                <div class="item-content">
-                    <h3>Titre du Premier Article</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt voluptatem adipisci qui consectetur tempora maiores voluptatibus obcaecati esse eius consequatur. Veritatis rem atque, laboriosam optio libero dolores eligendi ut rerum? Totam cum fugit nesciunt, ullam vero quis rerum beatae neque dolor exercitationem unde voluptate quibusdam reiciendis praesentium necessitatibus repudiandae animi.</p>
-                    <a href="#" class="btn secondary-btn">Lire la suite</a>
-                </div>
-            </article>
-
-            <article class="diaporama-item item-2">
-                <img src="images/natation.jpg" alt="Photo de l'article 2">
-                <div class="item-content">
-                    <h3>Titre du Deuxième Article</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quibusdam deserunt quo enim dolor placeat consequuntur aspernatur quam praesentium voluptate delectus, saepe necessitatibus repellendus? Ab facere aperiam dolor, itaque numquam aspernatur dolores consequuntur quae possimus unde sapiente sint id blanditiis nihil optio ea autem. Voluptatum tempore consectetur inventore exercitationem quidem.</p>
-                    <a href="#" class="btn secondary-btn">Détails</a>
-                </div>
-            </article>
-
+    <center>
+        <div class="main">
+            <form action="insert.php" method="post" enctype="multipart/form-data">
+                <h2>Les Jeux - la billetterie</h2>
+                <img src="logo.png" alt="logo" width="450px">
+                <input type="text" name='name'placeholder="Nom du Produit">
+                <br>
+                <input type="text" name='price'placeholder="Prix du produit">
+                <br>
+                <input type="file" id="file" name='image' style='display:none;'>
+                <label for="file"> Ajouter un photo de Produit</label>
+                <button name='upload'>Telecharger un Produit✅</button>
+                <br><br>
+                <a href="products.php">Afficher les Produits</a>
+            </form>
         </div>
-
-    </section>
-
-
-    <section class="page-content">
-
-        <h3>Bienvenue sur notre site</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci aperiam, aspernatur, nisi mollitia deserunt omnis laborum nostrum quisquam officiis nemo eius? Ipsa cumque enim a tempora ullam inventore vel accusamus! Eos, dignissimos unde iusto tenetur ea ab architecto necessitatibus in, impedit accusamus, cum iure obcaecati maxime cumque qui rerum labore? Quia suscipit consequatur sequi mollitia officiis modi sed in quam, magni, ex nesciunt facere aperiam nihil exercitationem doloribus quae quo et? Temporibus ex excepturi dolore sint itaque inventore error soluta perspiciatis. Iusto vero explicabo laborum debitis deserunt, deleniti illum eveniet minima nobis nam sapiente veritatis? Impedit sint deleniti cumque sit.</p>
-
-    </section>
-    <footer>
-
-    <div class="contact">
-            <h3>Contactez-nous :</h3>
-            <a href="#">📧 Email: contact@monsite.com</a>
-            <a href="#">📞 Téléphone: +33 1 23 45 67 89</a>
-            <a href="#">📍 Adresse: 123 Rue du Stade, 75001 Paris, France</a>
-    </div>
-    </footer>
-
+        <p>Developer By KIB</p>
+    </center>
 </body>
 </html>
--- products.php
+++ products.php
@@ -1,181 +1,41 @@
-<?php
-
-include 'config.php';
-session_start();
-$user_id = $_SESSION['user_id'];
-
-if(!isset($user_id)){
-   header('location:login.php');
-};
-
-if(isset($_GET['logout'])){
-   unset($user_id);
-   session_destroy();
-   header('location:login.php');
-};
-
-if(isset($_POST['add_to_cart'])){
-
-   $product_name = $_POST['product_name'];
-   $product_price = $_POST['product_price'];
-   $product_image = $_POST['product_image'];
-   $product_quantity = $_POST['product_quantity'];
-
-   $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
-
-   if(mysqli_num_rows($select_cart) > 0){
-      $message[] = 'Aricle ajouté ';
-   }else{
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, image, quantity) VALUES('$user_id', '$product_name', '$product_price', '$product_image', '$product_quantity')") or die('query failed');
-      $message[] = 'Aricle ajouté ';
-   }
-
-};
-
-if(isset($_POST['update_cart'])){
-   $update_quantity = $_POST['cart_quantity'];
-   $update_id = $_POST['cart_id'];
-   mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_quantity' WHERE id = '$update_id'") or die('query failed');
-   $message[] = 'Quantité Modifier';
-}
-
-if(isset($_GET['remove'])){
-   $remove_id = $_GET['remove'];
-   mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'") or die('query failed');
-   
-}
-  
-if(isset($_GET['delete_all'])){
-   mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
-   $message[] = 'Article';
-   
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Panier</title>
-
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amiri&family=Cairo:wght@200&family=Poppins:wght@100;200;300&family=Tajawal:wght@300&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/stylep.css">
+    <title>Products | Les Produits </title>
+    
 </head>
 <body>
-   
-<?php
-if(isset($message)){
-   foreach($message as $message){
-      echo '<div class="message" onclick="this.remove();">'.$message.'</div>';
-   }
-}
-?>
-
-<div class="container">
-
-<div class="user-profile">
-
-   <?php
-      $select_user = mysqli_query($conn, "SELECT * FROM `users` WHERE id = '$user_id'") or die('query failed');
-      if(mysqli_num_rows($select_user) > 0){
-         $fetch_user = mysqli_fetch_assoc($select_user);
-      };
-   ?>
-
-   <p>Bienvenue: <span><?php echo $fetch_user['name']; ?></span> </p>
-   <div class="flex">
-      <a href="index.php?logout=<?php echo $user_id; ?>" onclick="return confirm('voulez vous vous déconnecter?');" class="delete-btn">déconnecter</a>
-   </div>
-
-</div>
-
-<div class="products">
-
-   <h1 class="heading">Nos Offres</h1>
-
-   <div class="box-container">
-
-   <?php
-   include('config.php');
-   $result = mysqli_query($conn, "SELECT * FROM products");      
-   while($row = mysqli_fetch_array($result)){
-   ?>
-      <form method="post" class="box" action="">
-         <img src="admin/<?php echo $row['image']; ?>"  width="200">
-         <div class="name"><?php echo $row['name']; ?></div>
-         <div class="price"><?php echo $row['price']; ?></div>
-         <input type="number" min="1" name="product_quantity" value="1">
-         <input type="hidden" name="product_image" value="<?php echo $row['image']; ?>">
-         <input type="hidden" name="product_name" value="<?php echo $row['name']; ?>">
-         <input type="hidden" name="product_price" value="<?php echo $row['price']; ?>">
-         <input type="submit" value="ajouter au panier" name="add_to_cart" class="btn">
-      </form>
-   <?php
-      };
-   ?>
-
-   </div>
-
-</div>
-
-<div class="shopping-cart">
-
-   <h1 class="heading">Mon Panier</h1>
-
-   <table>
-      <thead>
-         <th>Offre</th>
-         <th>Nom</th>
-         <th>Prix</th>
-         <th>Quantité</th>
-         <th>Total</th>
-         <th>Panier</th>
-      </thead>
-      <tbody>
-      <?php
-         $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
-         $grand_total = 0;
-         if(mysqli_num_rows($cart_query) > 0){
-            while($fetch_cart = mysqli_fetch_assoc($cart_query)){
-      ?>
-         <tr>
-            <td><img src="admin/<?php echo $fetch_cart['image']; ?>" height="75" alt=""></td>
-            <td><?php echo $fetch_cart['name']; ?></td>
-            <td><?php echo $fetch_cart['price']; ?>€ </td>
-            <td>
-               <form action="" method="post">
-                  <input type="hidden" name="cart_id" value="<?php echo $fetch_cart['id']; ?>">
-                  <input type="number" min="1" name="cart_quantity" value="<?php echo $fetch_cart['quantity']; ?>">
-                  <input type="submit" name="update_cart" value="Modification" class="option-btn">
-               </form>
-            </td>
-            <td><?php echo $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']); ?>€</td>
-            <td><a href="products.php?remove=<?php echo $fetch_cart['id']; ?>" class="delete-btn" onclick="return confirm('article sera supprimer du Panier');">Supprimer</a></td>
-         </tr>
-      <?php
-         $grand_total += $sub_total;
-            }
-         }else{
-            echo '<tr><td style="padding:20px; text-transform:capitalize;" colspan="6"> Panier vide</td></tr>';
-         }
-      ?>
-      <tr class="table-bottom">
-         <td colspan="4">Prix total :</td>
-         <td><?php echo $grand_total; ?>€</td>
-         <td><a href="QR_code/index.php" onclick="return confirm('La commande sera confirmé');" class="btn btn-success <?php echo ($grand_total > 1)?'':'disabled'; ?>">Valider</a></td>
-      </tr>
-   </tbody>
-   </table>
-
-
-
-</div>
-
-</div>
-
+    <center>
+        <h3>Contrôle Administrateur</h3>
+    </center>
+    <?php
+    include('config.php');
+    $result = mysqli_query($con, "SELECT * FROM products");
+    while($row = mysqli_fetch_array($result)){
+        echo "
+        <center>
+        <main>
+            <div class='card' style='width: 15rem;'>
+                <img src='$row[image]' class='card-img-top'>
+                <div class='card-body'>
+                    <h5 class='card-title'>$row[name]</h5>
+                    <p class='card-text'>$row[price]</p>
+                    <a href='delete.php? id=$row[id]' class='btn btn-danger'>Supprimer</a>
+                    <a href='update.php? id=$row[id]' class='btn btn-primary'>Modifier</a>
+                </div>
+            </div>
+        </main>
+        <center>
+        ";
+    }
+    ?>
 </body>
 </html>
